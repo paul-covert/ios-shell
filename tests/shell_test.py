626bbd731f7c8e9760958772cfe5ad0a5aa81881
James Hannah <jhannah@protonmail.com>
2021-12-29 14:05:03 -0800

    remove functionally duplicate test

--- tests/shell_test.py
+++ tests/shell_test.py
@@ -1,61 +1,6 @@
 import pytest
 import os
 import ios_shell as shell
-import ios_shell.parsing as parsing
-
-TEST_FILES = [
-    "00200001.med",
-    "01100124.bot",
-    "1930-031-0001.bot",
-    "1930-031-0010.bot",
-    "1930-099-0001.bot",
-    "1930-003-0058.bot",
-    "1933-001-0001.bot",
-    "1933-001-0004.bot",
-    "1938-019-0001.bot",
-    "1950-001-0033.bot",
-    "1950-046-0002.bot",
-    "1952-002-0018.bot",
-    "1954-010-0001.bot",
-    "1954-010-0002.bot",
-    "1956-001-9001.bot",
-    "1958-004-0001.bot",
-    "1959-040-0013.bot",
-    "1985-041-0004.bot",
-    "1986-033-0003.bot",
-    "1987-070-0001.bot",
-    "1991-017-0003.bot",
-    "1991-017-0010.bot",
-    "1992-016-0001.bot",
-    "1994-031-0512.ctd",
-    "1996-036-0005.che",
-    "1996-038-0077.che",
-    "1997-033-0003.ctd",
-    "1998-015-8001.bot",
-    "2002-008-0026.ctd",
-    "2002-030-8008.bot",
-    "2002-043-Cape-Beale-Lighthouse.bot",
-    "2005-010-0016.che",
-    "2008-007-0002.tob",
-    "2008-010-0001.che",
-    "2008-026-0089.che",
-    "2015-022-0031.bot",
-    "51010002.ubc",
-    "74010003.ubc",
-    "BP2_20130705_20130810_0114m.ctd",
-    "cmp1_20160127_20160520_0193m.ctd",
-    "dev1_20150727_20160517_0152m.ctd",
-]
-
-
-@pytest.mark.parametrize(
-    "file_name",
-    TEST_FILES,
-)
-def test_shell_read_file(file_name):
-    full_file_name = os.path.join(os.path.dirname(__file__), "data", file_name)
-    info = shell.ShellFile.fromfile(full_file_name, process_data=True)
-    assert len(info.data) == info.file.number_of_records
 
 
 @pytest.mark.parametrize(
@@ -107,7 +52,49 @@ def test_shell_read_data(contents):
 
 @pytest.mark.parametrize(
     "file_name",
-    TEST_FILES,
+    [
+        "00200001.med",
+        "01100124.bot",
+        "1930-031-0001.bot",
+        "1930-031-0010.bot",
+        "1930-099-0001.bot",
+        "1930-003-0058.bot",
+        "1933-001-0001.bot",
+        "1933-001-0004.bot",
+        "1938-019-0001.bot",
+        "1950-001-0033.bot",
+        "1950-046-0002.bot",
+        "1952-002-0018.bot",
+        "1954-010-0001.bot",
+        "1954-010-0002.bot",
+        "1956-001-9001.bot",
+        "1958-004-0001.bot",
+        "1959-040-0013.bot",
+        "1985-041-0004.bot",
+        "1986-033-0003.bot",
+        "1987-070-0001.bot",
+        "1991-017-0003.bot",
+        "1991-017-0010.bot",
+        "1992-016-0001.bot",
+        "1994-031-0512.ctd",
+        "1996-036-0005.che",
+        "1996-038-0077.che",
+        "1997-033-0003.ctd",
+        "1998-015-8001.bot",
+        "2002-008-0026.ctd",
+        "2002-030-8008.bot",
+        "2002-043-Cape-Beale-Lighthouse.bot",
+        "2005-010-0016.che",
+        "2008-007-0002.tob",
+        "2008-010-0001.che",
+        "2008-026-0089.che",
+        "2015-022-0031.bot",
+        "51010002.ubc",
+        "74010003.ubc",
+        "BP2_20130705_20130810_0114m.ctd",
+        "cmp1_20160127_20160520_0193m.ctd",
+        "dev1_20150727_20160517_0152m.ctd",
+    ],
 )
 def test_shell_process_data(file_name):
     full_file_name = os.path.join(os.path.dirname(__file__), "data", file_name)
@@ -119,3 +106,4 @@ def test_shell_process_data(file_name):
 
     info = shell.ShellFile.fromfile(full_file_name, process_data=True)
     assert info.data_is_processed()
+    assert len(info.data) == info.file.number_of_records
